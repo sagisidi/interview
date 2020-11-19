6234bcbb1ccab28f1551e3fde11c44e7507bfe94
sagisidi <44197652+sagisidi@users.noreply.github.com>
2020-11-19 13:39:48 +0200

    upload build files

---
+++ build/precache-manifest.867fe1b316f32e41191e4d4dc4c770cc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ff7c2167763144fad2f6e8ff6776e0b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ddec8628e655168999bf",
+    "url": "/static/css/main.5e1e4623.chunk.css"
+  },
+  {
+    "revision": "cd0f512755a59f0a6ac3",
+    "url": "/static/js/2.0a4df64c.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/static/js/2.0a4df64c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5052aaa6effd4a80d351",
+    "url": "/static/js/3.fbd4c8b5.chunk.js"
+  },
+  {
+    "revision": "ddec8628e655168999bf",
+    "url": "/static/js/main.27bc9161.chunk.js"
+  },
+  {
+    "revision": "d82e80cee6a6b4fcd110",
+    "url": "/static/js/runtime-main.1759e668.js"
+  }
+]);
